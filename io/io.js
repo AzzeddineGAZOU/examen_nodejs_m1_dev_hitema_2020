27bde8952e59d924f6778f4fe07d007ab6bb7ad2
AzzeddineGAZOU <azdine.gazou@gmail.com>
2020-05-05 17:06:37 +0200

    websocket

--- io/io.js
+++ io/io.js
@@ -1,5 +1,13 @@
-module.exports.decodeHexFileContent = (filePath) =>  {
-    return new Promise((resolve, reject) => {
-        // To be implemented!
-    });
-}
+const fs = require("fs");
+
+module.exports.decodeHexFileContent = (filePath) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const fileContent = fs.readFileSync(filePath).toString();
+      const decodedContent = Buffer.from(fileContent, "hex").toString();
+      resolve(decodedContent);
+    } catch (error) {
+      reject();
+    }
+  });
+};
