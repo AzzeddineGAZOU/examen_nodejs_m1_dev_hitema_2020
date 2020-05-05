6ec4162b27c495ec5715b81b63621b8a09d0ce4b
AzzeddineGAZOU <azdine.gazou@gmail.com>
2020-05-05 16:40:12 +0200

    api rest +services

--- api/api.js
+++ api/api.js
@@ -1,9 +1,35 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const HttpStatus = require("http-status-codes");
+const PeopleService = require("./people-service");
 
-const PeopleService = require('./people-service');
-const peopleService = new PeopleService();
 const app = express();
+const v1 = express.Router();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use("/api/v1", v1);
+
+const peopleService = new PeopleService();
 
 // To be implemented!
+v1.put("/people/:id", (request, response) => {
+  const id = request.params.id;
+  const people = request.body;
+
+  const potentialUpdatedPeople = peopleService.updatePeople(id, people);
+
+  if (potentialUpdatedPeople) {
+    return response.sendStatus(HttpStatus.OK);
+  } else {
+    return response.sendStatus(HttpStatus.NOT_FOUND);
+  }
+});
+
+v1.get("/people", (request, response) => {
+  const filter = request.query;
+  return response.send(peopleService.getPeople(filter));
+});
 
 module.exports = app;
